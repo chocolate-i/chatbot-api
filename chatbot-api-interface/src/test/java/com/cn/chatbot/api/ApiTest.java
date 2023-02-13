d7c27946e5dd30900cb3a92c487e135f3f03e66b
anan <anan@qq.com>
2023-02-13 22:23:27 +0800

    start ready

---
+++ chatbot-api-interface/src/test/java/com/cn/chatbot/api/ApiTest.java
@@ -0,0 +1,12 @@
+package com.cn.chatbot.api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTest {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTest.class,args);
+    }
+}
